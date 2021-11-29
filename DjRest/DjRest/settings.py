44f30bdb361dba695bb74f9d0de44c524963fe7c
Polly-coder <ushakovapa@gmail.com>
2021-11-29 15:30:12 +0300

    Add docker and db

--- DjRest/DjRest/settings.py
+++ DjRest/DjRest/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'DjRest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
@@ -103,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'rus'
 
 TIME_ZONE = 'UTC'
 
